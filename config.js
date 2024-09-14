c5051ec4e5a286498a6255dd641761bd1a46a844
Shadowmd99 <shadowwatsappbot@gmail.com>
2024-09-14 15:35:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/99312f6ecbfa4d9bfd3b4.jpg",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+};
